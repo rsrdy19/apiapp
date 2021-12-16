6790f1b45501799d6842a18b1a5082ef15d834f0
rsrdy19 <96263222+rsrdy19@users.noreply.github.com>
2021-12-17 02:13:08 +0530

    Create data_app.py

---
+++ app/data_app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, jsonify
+import sqlite3 as sql
+import json
+
+app = Flask(__name__)
+
+# @app.route('/users/<idl>/')
+# def test(idl=None):
+#     g.db = connect_db()
+#     cur = g.db.execute('select id, name from users where id = ?', [idl])
+#     rows = cur.fetchall();
+#     #der = [dict(id=row[0], posts=row[1]) for row in cur.fetchall()]
+#     g.db.close()
+#     return render_template('test.html', rows=rows)
+#
+#
+# def connect_db():
+#     return sqlite3.connect('users.db')
+
+@app.route('/', methods=["GET"])
+def liist():
+    con = sql.connect("users.db")
+    con.row_factory = sql.Row
+    
+    cur = con.cursor()
+    cur.execute("select * from users")
+    
+    d = cur.fetchall();
+    print(jsonify(d))
+    return jsonify(d)
